970328c3ebe291bf918e7dbb19c2efd2b7ebca1d
Said Silva <saidsilva1106@gmail.com>
2023-06-22 22:50:33 -0600

    TEST NIDAQ+ARD

--- src/pythonScripts/nidaqTest.py
+++ src/pythonScripts/nidaqTest.py
@@ -20,44 +20,50 @@ def funcion_a_ejecutar():
     tiempo_inicial = time.time()
     while time.time() - tiempo_inicial < duracion:
         with nidaqmx.Task() as task:
-            if(cantidadEmgs >= 1):
+                
                 task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
-            # line = task.read()
-            # returnedList.append({"EMG1": line})
-            if(cantidadEmgs >= 2):
-
                 task.ai_channels.add_ai_voltage_chan("Dev1/ai1")
-            # line = task.read()
-            # returnedList.append({"EMG2": line})
-            if(cantidadEmgs >= 3):
                 task.ai_channels.add_ai_voltage_chan("Dev1/ai2")
-            # line = task.read()
-            # returnedList.append({"EMG3": line})
-            if(cantidadEmgs >= 4):
                 task.ai_channels.add_ai_voltage_chan("Dev1/ai3")
+                line = task.read()
+
+                returnedList.append({"EMG1": line[0]})
+                returnedList.append({"EMG2": line[1]})  
+                returnedList.append({"EMG3": line[2]})  
+                returnedList.append({"EMG4": line[3]})  
+
+
+
+
+            # if(cantidadEmgs >= 1):
+            #     task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+            # if(cantidadEmgs >= 2):
+            #     task.ai_channels.add_ai_voltage_chan("Dev1/ai1")
+            # if(cantidadEmgs >= 3):
+            #     task.ai_channels.add_ai_voltage_chan("Dev1/ai2")
+            # if(cantidadEmgs >= 4):
+            #     task.ai_channels.add_ai_voltage_chan("Dev1/ai3")
             # line = task.read()
-            # returnedList.append({"EMG4": line})
-            line = task.read()
 
-            if(cantidadEmgs >= 1):
-                returnedList.append({"EMG1": line[0]})  
-                oldEmg1List = returnedDict['emg1']
-                returnedDict['emg1'] = oldEmg1List.append(line[0])
+            # if(cantidadEmgs >= 1):
+            #     returnedList.append({"EMG1": line[0]})  
+            #     oldEmg1List = returnedDict['emg1']
+            #     returnedDict['emg1'] = oldEmg1List.append(line[0])
 
-            if(cantidadEmgs >= 2):
-                returnedList.append({"EMG2": line[1]})
-                oldEmg2List = returnedDict['emg2']
-                returnedDict['emg2'] = oldEmg2List.append(line[1])
+            # if(cantidadEmgs >= 2):
+            #     returnedList.append({"EMG2": line[1]})
+            #     oldEmg2List = returnedDict['emg2']
+            #     returnedDict['emg2'] = oldEmg2List.append(line[1])
 
-            if(cantidadEmgs >= 3):
-                returnedList.append({"EMG3": line[2]})
-                oldEmg3List = returnedDict['emg3']
-                returnedDict['emg3'] = oldEmg3List.append(line[2])
+            # if(cantidadEmgs >= 3):
+            #     returnedList.append({"EMG3": line[2]})
+            #     oldEmg3List = returnedDict['emg3']
+            #     returnedDict['emg3'] = oldEmg3List.append(line[2])
 
-            if(cantidadEmgs >= 4):
-                returnedList.append({"EMG4": line[3]})
-                oldEmg4List = returnedDict['emg4']
-                returnedDict['emg4'] = oldEmg4List.append(line[3])
+            # if(cantidadEmgs >= 4):
+            #     returnedList.append({"EMG4": line[3]})
+            #     oldEmg4List = returnedDict['emg4']
+            #     returnedDict['emg4'] = oldEmg4List.append(line[3])
 
         # print("reading", valores)
         # tiempo_actual = time.time()
